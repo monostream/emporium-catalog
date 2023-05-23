eee72f46662cb4497e25697d22fb61a2a6c293bd
Christian <christian@monostream.com>
2023-05-23 15:27:36 +0200

    add download external script

---
+++ download-external.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+set -o noclobber
+set -o noglob
+
+# external charts to be downloaded
+charts=("ghost^19.3.6")
+
+for chart in "${charts[@]}"
+do
+  # parts=$(echo "$chart" | tr '^' '\n')
+  IFS="^" read -ra parts <<< "$chart"
+  name="${parts[0]}"
+  version="${parts[1]}"
+
+  echo "downloading $name in version ^$version"
+
+  helm pull bitnami/$name --destination charts/$name --version ^$version
+
+  echo "extracting $name to charts/$name"
+
+  find charts/$name -name "$name*" -type f -print0 | xargs -0 -I {} tar -xzf {} -C charts
+done
