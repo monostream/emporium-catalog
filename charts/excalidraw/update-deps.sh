22416d5ecdfe15f7c364177913fb625f9f77f79b
Marco De Luca <marcodl404@gmail.com>
2023-12-28 17:05:30 +0100

    add update script to more charts

---
+++ charts/excalidraw/update-deps.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+CLI_EXISTS=$(which depschecker)
+
+if [[ -z ${CLI_EXISTS} ]]; then
+  echo "ERROR: depschecker not found"
+  exit 1
+fi
+
+HELM_EXISTS=$(which helm)
+
+if [[ -z ${HELM_EXISTS} ]]; then
+  echo "ERROR: helm not found"
+  exit 1
+fi
+
+depschecker chart ./chart --image-tag image.tag --update
